bb97931c350b37d49f625988253e433d173c21ea
mike@kaslingaircraft.com <mike@kaslingaircraft.com>
2016-01-12 15:59:17 -0600

    Add time functions

--- src/I2CScan/I2Ctest.py
+++ src/I2CScan/I2Ctest.py
@@ -1,14 +1,42 @@
 import Adafruit_GPIO.FT232H as FT232H
 import struct
+import time
+
+from datetime import datetime
+from datetime import timedelta
+from math import floor
+
+print (timedelta.resolution)
 # Temporarily disable FTDI serial drivers.
 FT232H.use_FT232H()
 
 # Find the first FT232H device.
 ft232h = FT232H.FT232H()
 
+startTime = datetime.now()
+#print time.get_clock_info('clock')
+
+def millis():
+   return floor(time.clock() * 1000)
+def micros():
+    return floor(time.clock() * 1000000)
+def nanos():
+    return floor(time.clock() * 1000000000)
+X_ = 0
+Y_ = 1
+Z_ = 2
+
 #gyro defines - ST L3G2
 READ = 0x80
-
+for x in range(10000):
+    #print time.clock()
+    #print floor(time.clock() * 1000000)
+    print time.clock()
+    print nanos()
+    print millis()
+    print micros()
+    
+    
 L3G_CTRL_REG1 = 0x20
 L3G_CTRL_REG2 = 0x21
 L3G_CTRL_REG3 = 0x22
@@ -32,13 +60,11 @@ L3G_I2C_ADDR = 0x69
 #          
 #   
 # print 'Done!'
-
 gyro = FT232H.I2CDevice(ft232h,L3G_I2C_ADDR)
 
 whoAmI = gyro.readU8(L3G_WHO_AM_I)
-
-# print format(whoAmI,'02x')
-# print bin(whoAmI)
+format(whoAmI,'02x')
+print (bin(whoAmI))
 
 gyro.write8(L3G_CTRL_REG2, 0x00)
 gyro.write8(L3G_CTRL_REG3, 0x00)
@@ -54,30 +80,26 @@ gyro.write8(L3G_CTRL_REG1, 0x8F)
 # print format(L3G_OUT_X_L | READ,'02x')
 
 xVal3 = gyro.readS16(L3G_OUT_X_L | READ, True)
-# 
 # print xVal3
 #  
 # print bin(xVal3)
 # 
-testList = gyro.readList(L3G_OUT_X_L | READ, 6)
+gyroByteList = gyro.readList(L3G_OUT_X_L | READ, 6)
 
-print format(testList[0],'02x')
-print format(testList[1],'02x')
-print format(testList[2],'02x')
+print (format(gyroByteList[0],'02x'))
+print (format(gyroByteList[1],'02x'))
+print (format(gyroByteList[2],'02x'))
 
-xVal = struct.unpack('h',testList[0:2])
-
-print xVal[0]
-
-x = testList[1]<<8 | testList[0]
-
-print x 
-
-if x > 32767:
-    x = x - 65536
+gyroIntList = struct.unpack('hhh',gyroByteList[0:6])
+print (gyroIntList)
 
 
-print x 
+xGyro = gyroIntList[0]
+yGyro = gyroIntList[1]
+zGyro = gyroIntList[2]
+
+print (xGyro,",",yGyro,",",zGyro)
+
 
 
 
